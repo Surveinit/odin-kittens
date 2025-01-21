0011d8b80d1434c0c77a8f6b265ab9785687d920
Surve <surve8729@gmail.com>
2025-01-21 16:39:58 +0530

    Kitten new & edit action

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -8,6 +8,33 @@ class KittensController < ApplicationController
   end
 
   def new
-  @kitten = Kitten.new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to(@kitten)
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:name])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:name])
+    if @kitten.update(kitten_params)
+      redirect_to(@kitten)
+    else
+      render(:edit, status: :unprocessable_entity)
+    end
+  end
+
+  private
+  def kitten_params
+    params.expect(kitten: [ :name ])
   end
 end
